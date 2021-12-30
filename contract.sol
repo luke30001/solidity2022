ce3c53bf007972601df071489e5c97abb02968fd
luke30001 <41565281+luke30001@users.noreply.github.com>
2021-12-30 23:41:27 +0100

    Create contract.sol

---
+++ contract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/** 
+ * @title Ballot
+ * @dev Implements voting process along with vote delegation
+ */
+contract moneyether {
+    function hasher(string memory data) public pure returns(bytes32){
+        return keccak256(abi.encode(data));
+    }
+}
